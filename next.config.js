e68aefb8e50651ed97714a61a0b67a3cb775610b
mariyakj <151432612+mariyakj@users.noreply.github.com>
2025-01-18 22:42:15 +0530

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true }, // Keep this if your images don't require optimization
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
